1d8af7e5b23e1d80bbfc6be18cb1e29fa6b49f25
Djordje Knezevic <starwarrior@mycity.rs>
2022-10-20 18:20:38 +0200

    Create Story struct, and use it to create array of stories and display first in ViewController

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  Destini-iOS13
-//
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
 
 import UIKit
 
@@ -14,23 +7,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    let story0 = "You see a fork in the road."
-    let choice1 = "Take a left."
-    let choice2 = "Take a right."
+    let stories = [Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+                   Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+                   Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")]
     
     @IBAction func choiceMade(_ sender: UIButton) {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        storyLabel.text = story0
+        storyLabel.text = stories[0].title
         
         updateUI()
     }
     
     func updateUI() {
-        choice1Button.setTitle(choice1, for: .normal)
-        choice2Button.setTitle(choice2, for: .normal)
+        choice1Button.setTitle(stories[0].choice1, for: .normal)
+        choice2Button.setTitle(stories[0].choice2, for: .normal)
     }
 
 }
