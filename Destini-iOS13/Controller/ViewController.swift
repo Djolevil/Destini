f5f310e971d58e289ef867dcaebc932b55dbad9b
Djordje Knezevic <starwarrior@mycity.rs>
2022-10-25 16:54:21 +0200

    Update Story struct with choice destination properties, make StoryBrain more scalable, remove hardcoded part in ViewController and finish project

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -15,16 +15,14 @@ class ViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        storyLabel.text = storyBrain.stories[storyBrain.storyNumber].title
-        
+                
         updateUI()
     }
     
     func updateUI() {
-        storyLabel.text = storyBrain.stories[storyBrain.storyNumber].title
-        choice1Button.setTitle(storyBrain.stories[storyBrain.storyNumber].choice1, for: .normal)
-        choice2Button.setTitle(storyBrain.stories[storyBrain.storyNumber].choice2, for: .normal)
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
     }
 
 }
